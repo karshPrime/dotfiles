872450afb5d83e21a354cd58e4bb06711fac19f7
karsh <utkarsh@cyberfear.com>
2024-05-02 02:24:58 +1000

    layout update
    + added support for view @recording on lualine when working with macros
    ~ fixed indentation

--- config/nvim/lua/plugins/layout.lua
+++ config/nvim/lua/plugins/layout.lua
@@ -23,9 +23,17 @@ vim.cmd.colorscheme("ayu-dark")
 --# Statusline #------------------------------------------------------------------------
 
 require('lualine').setup({
-  options = {
-    theme = 'ayu',
-  },
+    options = {
+        theme = 'ayu',
+    },
+
+    sections = {
+        lualine_x = { {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            color = { fg = "#ff9e64" },
+        } },
+    },
 })
 
 -- border line colour
@@ -35,20 +43,23 @@ vim.cmd('highlight VertSplit guifg=#ff5f87 guibg=NONE ctermfg=160 ctermbg=NONE')
 --# noice #-----------------------------------------------------------------------------
 
 require("noice").setup({
-  lsp = {
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use
+        -- **Treesitter**
+        override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
     },
-  },
-  -- you can enable a preset for easier configuration
-  presets = {
-    bottom_search = true, -- use a classic bottom cmdline for search
-    command_palette = true, -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false, -- add a border to hover docs and signature help
-  },
+
+    -- you can enable a preset for easier configuration
+    presets = {
+        bottom_search = true,           -- use a classic bottom cmdline for search
+        command_palette = true,         -- position the cmdline and popupmenu together
+        long_message_to_split = true,   -- long messages will be sent to a split
+        inc_rename = false,             -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false,         -- add a border to hover docs and signature help
+    },
 })
+
