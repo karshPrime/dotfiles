47b1d4e772a06adeb2bd68f5236541d89ccea2b3
karsh <utkarsh@cyberfear.com>
2024-06-25 13:29:35 +1000

    tmux-compile update
    ~ updated plugin configs and keybindings with accordance to new update

--- shared/nvim/lua/plugins/tmux.lua
+++ shared/nvim/lua/plugins/tmux.lua
@@ -4,36 +4,35 @@
 --# TMUX Compile #-------------------------------------------------------------
 
 require('tmux-compile').setup({
+    save_session = true,
+    sleep_duration = 0,
     overlay_width_percent = 95,
     overlay_height_percent = 95,
-    build_run_config = {
-        {
-            extension = {'c', 'cpp', 'h'},
-            build = 'make',
-            run = 'make run',
-        },
-        {
-            extension = {'rs'},
-            build = 'cargo build',
-            run = 'cargo run',
-        },
-        {
-            extension = {'go'},
-            build = 'go build',
-            run = 'go run .',
-        }
-    }
+    build_run_config = {{
+        extension = {'c', 'cpp', 'h'},
+        build = 'make',
+        run = 'make run',
+        debug = 'lldb',
+    },{
+        extension = {'rs'},
+        build = 'cargo build',
+        run = 'cargo run',
+    },{
+        extension = {'go'},
+        run = 'go run .',
+    }}
 })
 
-vim.keymap.set('n','<leader>g', ':w<CR> :TMUXcompile lazygit<CR>', {silent=true})
+vim.keymap.set('n','<leader>g', ':TMUXcompile lazygit<CR>', {silent=true})
 
-vim.keymap.set('n','<leader>`', ':w<CR> :TMUXcompile Run<CR>',{silent=true})
-vim.keymap.set('n','[]<leader>', ':w<CR> :TMUXcompile RunBG<CR>',{silent=true})
-vim.keymap.set('n','<leader>]', ':w<CR> :TMUXcompile RunV<CR>', {silent=true})
-vim.keymap.set('n','<leader>[', ':w<CR> :TMUXcompile RunH<CR>', {silent=true})
+vim.keymap.set('n','<leader>`', ':TMUXcompile Run<CR>'    , {silent=true})
+vim.keymap.set('n','[]<leader>', ':TMUXcompile RunBG<CR>' , {silent=true})
+vim.keymap.set('n','<leader>]', ':TMUXcompile RunV<CR>'   , {silent=true})
+vim.keymap.set('n','<leader>[', ':TMUXcompile RunH<CR>'   , {silent=true})
 
-vim.keymap.set('n','`<leader>`', ':w<CR> :TMUXcompile Make<CR>', {silent=true})
-vim.keymap.set('n','][<leader>', ':w<CR> :TMUXcompile MakeBG<CR>', {silent=true})
+vim.keymap.set('n','<leader>d', ':TMUXcompile Debug<CR>'  , {silent=true})
+vim.keymap.set('n','`<leader>`', ':TMUXcompile Make<CR>'  , {silent=true})
+vim.keymap.set('n','][<leader>', ':TMUXcompile MakeBG<CR>', {silent=true})
 
 --# Only TMUX #----------------------------------------------------------------
 
