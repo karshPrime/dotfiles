5c5daf4d71a6a2e02f28c1159a44323b75c4fd4f
karsh <utkarsh@cyberfear.com>
2024-07-07 11:35:01 +1000

    install script
    ~ updated install script to be "more complete" with setting up different
      systems
    ~ renamed zsh plugins, removed "zsh-" from their name
    - removed lf symlink from darwin and moved it from shared/ to server/
    ~ not all users should share some configs, and if they do then updating
      /etc/ would be more appropriate than using /base0/

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#--# Helper #-----------------------------------------------------------------------------
+
 # more consistent errors
 error() {
     echo -e "\033[31mError: \033[0m$1"
@@ -12,60 +14,61 @@ if [ $# -eq 0 ]; then
     error "No argument provided"
 fi
 
-# create symlinks for all dirs in ~/.config
-create_symlinks() {
-    local source_path=$1
-    local remote_path="$HOME/.config"
-
-    # check if .config exists, if not create it
-    if [ ! -d "$remote_path" ]; then
-        mkdir -p "$remote_path"
-    fi
-
-    # iterate over files and directories in source path
-    for item in "$source_path"/*; do
-        base_name=$(basename "$item")
-
-        # skip README files
-        if [[ "$base_name" == "README.md" ]]; then
-            continue
-        fi
-
-        target="$remote_path/$base_name"
-
-        # if target already exists, rename it to name.OLD
-        if [ -e "$target" ]; then
-            mv "$target" "$target.OLD"
-        fi
-
-        # create symbolic link in remote_path
-        ln -s "$item" "$target"
-    done
-
-    echo "System configured successfully."
+# create symlink and preserve existing
+symlink() {
+    mv "$HOME/.config/$2" "$HOME/.config/$2.old" 2>/dev/null
+    ln -s "$1/$2" "$HOME/.config/$2"
 }
+
+
+#--# Initialise Repository #-------------------------------------------------------------
+
+mkdir -p $HOME/.config
+mv "$HOME/.config/dotfiles" "$HOME/.config/dotfiles.old" 2>/dev/null
+git clone --depth=1 "https://github.com/karshPrime/dotfiles" $HOME/.config/dotfiles
+
+
+#--# System Specifics #------------------------------------------------------------------
 
 case $1 in
     darwin)
         echo "Configuring Darwin"
+        CONFIGS="$HOME/.config/dotfiles/darwin"
         git sparse-checkout set darwin shared
-        if [ $? -ne 0 ]; then
-            error "Failed to configure git sparse-checkout for Darwin."
-        fi
-        create_symlinks ./darwin
+
+        symlink "$CONFIGS" "alacritty" # alacritty
         ;;
 
     server)
         echo "Configuring Server"
+        CONFIGS="$HOME/.config/dotfiles/server"
         git sparse-checkout set server shared
-        if [ $? -ne 0 ]; then
-            error "Failed to configure git sparse-checkout for Server."
-        fi
-        create_symlinks ./server
+
+        symlink "$CONFIGS" "lf"    # lf
         ;;
 
     *)
         error "Unrecognized config $1"
+        exit 1
         ;;
 esac
+
+
+#--# SymLinks #--------------------------------------------------------------------------
+
+symlink "$CONFIGS" "nvim"          # Neovim Editor
+symlink "$CONFIGS" "tmux"          # tmux multiplexer
+symlink "$CONFIGS" "starship.toml" # Starship Prompt
+symlink "$CONFIGS" "fetch.sh"      # Terminal Fetch
+
+# Z SHell
+mv "$HOME/.zshrc" "$HOME/.zshrc.old" 2>/dev/null
+ln -s "$CONFIGS/zsh/zshrc" "$HOME/.zshrc"
+## plugins
+mv "$HOME/.config/zsh" "$HOME/.config/zsh.old" 2>/dev/null
+mkdir "$HOME/.config/zsh"
+cd $HOME/.config/zsh
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search.git history
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git highlighting
 
