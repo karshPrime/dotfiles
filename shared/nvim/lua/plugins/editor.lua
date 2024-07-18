11d2607d045e448780d6b2c3986bae9003ba1cd2
karsh <utkarsh@cyberfear.com>
2024-07-18 21:16:25 +1000

    surround text action
    + added plugin to make changes around text

--- shared/nvim/lua/plugins/editor.lua
+++ shared/nvim/lua/plugins/editor.lua
@@ -11,3 +11,18 @@ vim.o.signcolumn = 'yes'          -- Always show the sign column (gutter)
 
 require('Comment').setup()
 
+
+--# Surround Actions #---------------------------------------------------------
+
+require("nvim-surround").setup()
+
+--     Old text                    Command         New text
+-- --------------------------------------------------------------------------------
+--     surr*ound_words             ysiw)           (surround_words)
+--     *make strings               ys$"            "make strings"
+--     [delete ar*ound me!]        ds]             delete around me!
+--     remove <b>HTML t*ags</b>    dst             remove HTML tags
+--     'change quot*es'            cs'"            "change quotes"
+--     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+--     delete(functi*on calls)     dsf             function calls
+
--- shared/nvim/lua/plugins/init.lua
+++ shared/nvim/lua/plugins/init.lua
@@ -2,50 +2,53 @@
 --# init.lua #-----------------------------------------------------------------
 
 local plugins = {
-    -- CMP
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    { 'hrsh7th/cmp-cmdline' },
-    { 'hrsh7th/nvim-cmp' },
+	-- CMP
+	{ 'hrsh7th/cmp-nvim-lsp' },
+	{ 'hrsh7th/cmp-buffer' },
+	{ 'hrsh7th/cmp-path' },
+	{ 'hrsh7th/cmp-cmdline' },
+	{ 'hrsh7th/nvim-cmp' },
 
-    -- EDITOR
-    { 'airblade/vim-gitgutter' },                       -- git column
-    { 'numToStr/Comment.nvim', event = 'VeryLazy' },    -- easier commenting
+	-- EDITOR
+	{ 'airblade/vim-gitgutter' },						-- git column
+	{ 'numToStr/Comment.nvim', event = 'VeryLazy' },	-- easier commenting
+	{ 'kylechui/nvim-surround', version = "*",			-- more surround actions
+	   event = "VeryLazy",
+	},
 
-    -- LAYOUT
-    { 'nvim-lualine/lualine.nvim' },                    -- statusbar
-    { 'karshPrime/tokyoburn.nvim' },                    -- color theme
-    { 'folke/noice.nvim', event = 'VeryLazy',           -- message UI
-       dependencies = { 'MunifTanjim/nui.nvim' }
-    },
+	-- LAYOUT
+	{ 'nvim-lualine/lualine.nvim' },					-- statusbar
+	{ 'karshPrime/tokyoburn.nvim' },					-- color theme
+	{ 'folke/noice.nvim', event = 'VeryLazy',			-- message UI
+	   dependencies = { 'MunifTanjim/nui.nvim' }
+	},
 
-    -- LSP
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
-    { 'neovim/nvim-lspconfig' },
-    { 'https://git.sr.ht/~whynothugo/lsp_lines.nvim' }, -- multiline errors
+	-- LSP
+	{ 'williamboman/mason.nvim' },
+	{ 'williamboman/mason-lspconfig.nvim' },
+	{ 'neovim/nvim-lspconfig' },
+	{ 'https://git.sr.ht/~whynothugo/lsp_lines.nvim' }, -- multiline errors
 
-    -- NO CONFIG
-    { 'nvim-tree/nvim-web-devicons', lazy = true },     -- icons
-    { 'mg979/vim-visual-multi', event = 'VeryLazy' },   -- multi cursors (ctrl+n)
+	-- NO CONFIG
+	{ 'nvim-tree/nvim-web-devicons', lazy = true },		-- icons
+	{ 'mg979/vim-visual-multi', event = 'VeryLazy' },	-- ctrl+n multi cursor
 
-    -- NAVIGATE
-    { 'nvim-tree/nvim-tree.lua' },                      -- file tree
-    { 'ThePrimeagen/harpoon', event = 'VeryLazy',       -- project wise marks
-       branch = "harpoon2"
-    },
-    { 'nvim-telescope/telescope.nvim', tag = '0.1.8',   -- telescope
-       dependencies = { 'nvim-lua/plenary.nvim' },
-       event = 'VeryLazy'
-    },
+	-- NAVIGATE
+	{ 'nvim-tree/nvim-tree.lua', event = 'VeryLazy' },	-- file tree
+	{ 'ThePrimeagen/harpoon', event = 'VeryLazy',		-- project wise marks
+	   branch = "harpoon2"
+	},
+	{ 'nvim-telescope/telescope.nvim', tag = '0.1.8',   -- telescope
+	   dependencies = { 'nvim-lua/plenary.nvim' },
+	   event = 'VeryLazy'
+	},
 
-    -- TMUX
-    { 'christoomey/vim-tmux-navigator' },               -- vim tmux keybinds
-    { 'karshPrime/only-tmux.nvim', event = 'VeryLazy' },-- tmux panels :only
-    { 'karshPrime/tmux-compile.nvim',                   -- execute in tmux
-       event = 'VeryLazy'
-    },
+	-- TMUX
+	{ 'christoomey/vim-tmux-navigator' },				-- vim tmux keybinds
+	{ 'karshPrime/only-tmux.nvim', event = 'VeryLazy' },-- tmux panels :only
+	{ 'karshPrime/tmux-compile.nvim',					-- execute in tmux
+	   event = 'VeryLazy'
+	},
 }
 
 local opts = {}
